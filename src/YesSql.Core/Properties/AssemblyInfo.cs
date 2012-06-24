b39abcc3dd61bb685c6686f03c02e8c4ffc230c9
sebastienros <sebastienros@gmail.com>
2012-06-24 13:43:39 -0700

    Updating version

--- src/YesSql.Core/Properties/AssemblyInfo.cs
+++ src/YesSql.Core/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyCompany("Sébastien Ros")]
 [assembly: AssemblyCopyright("Sébastien Ros")]
 
-[assembly: AssemblyVersion("0.3.3")]
+[assembly: AssemblyVersion("0.4.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
