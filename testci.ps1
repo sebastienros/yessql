6cef82d0390a0c2f38ccefd30e5eba4827ef0fef
Sebastien Ros <sebastienros@gmail.com>
2016-02-17 12:58:07 -0800

    Forcing environment variable

--- testci.ps1
+++ testci.ps1
@@ -1,4 +1,4 @@
- # $env:DBCONNECTIONSTRING = "Server=(local)\\SQL2014;Database=master;User ID=sa;Password=Password12!"
+ $env:DBCONNECTIONSTRING = "Server=(local)\\SQL2014;Database=master;User ID=sa;Password=Password12!"
 
  & cd .\src\YesSql.Tests
  & dnx test
