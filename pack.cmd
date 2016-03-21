0dc941f09a5d81d66d15a1ef6b241b733aaf8579
Alexander Bocharov <alex.bocharov@outlook.com>
2016-03-21 15:18:06 +0300

    Packing using dotnet cli

--- pack.cmd
+++ pack.cmd
@@ -1,6 +1,6 @@
 del .\artifacts\packages /s /f /q
-CALL dnu pack .\src\YesSql.Core --configuration release --out .\artifacts\packages\YesSql.Core
-CALL dnu pack .\src\YesSql.Storage.InMemory --configuration release --out .\artifacts\packages\YesSql.Storage.InMemory
-CALL dnu pack .\src\YesSql.Storage.Cache --configuration release --out .\artifacts\packages\YesSql.Storage.Cache
-CALL dnu pack .\src\YesSql.Storage.LightningDB --configuration release --out .\artifacts\packages\YesSql.Storage.LightningDB
-CALL dnu pack .\src\YesSql.Storage.Sql --configuration release --out .\artifacts\packages\YesSql.Storage.Sql
+CALL dotnet pack .\src\YesSql.Core --configuration release --out .\artifacts\packages\YesSql.Core
+CALL dotnet pack .\src\YesSql.Storage.InMemory --configuration release --out .\artifacts\packages\YesSql.Storage.InMemory
+CALL dotnet pack .\src\YesSql.Storage.Cache --configuration release --out .\artifacts\packages\YesSql.Storage.Cache
+CALL dotnet pack .\src\YesSql.Storage.LightningDB --configuration release --out .\artifacts\packages\YesSql.Storage.LightningDB
+CALL dotnet pack .\src\YesSql.Storage.Sql --configuration release --out .\artifacts\packages\YesSql.Storage.Sql
