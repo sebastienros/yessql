8727fbbfb7042f43581787ea8eaa04dd035ac2ec
Sebastien Ros <sebastienros@gmail.com>
2016-09-21 16:17:43 -0700

    Creating collection classes

---
+++ src/YesSql.Core/Collections/Collection.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Text;
+
+namespace YesSql.Core.Collections
+{
+    public abstract class Collection : IDisposable
+    {
+        IDisposable _scope;
+
+        public Collection()
+        {
+            _scope = CollectionHelper.EnterScope(this);
+        }
+
+        public abstract string GetSafeName();
+        public static string CreateSafeName(string name)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            var builder = new StringBuilder();
+            foreach (var c in name)
+            {
+                if (Char.IsLetter(c))
+                {
+                    builder.Append(c);
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        public void Dispose()
+        {
+            _scope.Dispose();
+        }
+    }
+
+    public static class CollectionExtensions
+    {
+        public static string GetPrefixedName(this Collection collection, string table)
+        {
+            var name = collection.GetSafeName();
+
+            if (String.IsNullOrEmpty(name))
+            {
+                return table;
+            }
+
+            return String.Concat(name, "_", table);
+        }
+    }
+
+    public class DefaultCollection : Collection
+    {
+        public DefaultCollection()
+        {
+
+        }
+
+        public override string GetSafeName()
+        {
+            return "";
+        }
+    }
+
+    public class TypeCollection<T> : Collection
+    {
+        private readonly string _safeName;
+
+        public TypeCollection()
+        {
+            _safeName = CreateSafeName(typeof(T).Name);
+
+            if(String.IsNullOrEmpty(_safeName))
+            {
+                throw new ArgumentException("Invalid collection name: " + _safeName);
+            }
+        }
+
+        public override string GetSafeName()
+        {
+            return _safeName;
+        }
+    }
+
+    public class NamedCollection : Collection
+    {
+        private readonly string _safeName;
+
+        public NamedCollection(string name)
+        {
+            _safeName = CreateSafeName(name);
+
+            if (String.IsNullOrEmpty(_safeName))
+            {
+                throw new ArgumentException("Invalid collection name: " + _safeName);
+            }
+        }
+
+        public override string GetSafeName()
+        {
+            return _safeName;
+        }
+    }
+}
---
+++ src/YesSql.Core/Collections/CollectionStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace YesSql.Core.Collections
+{
+    internal class CollectionStack
+    {
+        private readonly CollectionStack _previous;
+        private readonly Collection _collection;
+
+        static CollectionStack()
+        {
+            Empty = new CollectionStack().Push(new DefaultCollection());
+        }
+
+        private CollectionStack()
+        {
+        }
+
+        private CollectionStack(CollectionStack previous, Collection collection)
+        {
+            if (previous == null)
+            {
+                throw new ArgumentNullException(nameof(previous));
+            }
+
+            _previous = previous;
+            _collection = collection;
+        }
+
+        public readonly static CollectionStack Empty;
+
+        public CollectionStack Push(Collection c)
+        {
+            return new CollectionStack(this, c);
+        }
+
+        public Collection Peek()
+        {
+            return _collection;
+        }
+    }
+}
