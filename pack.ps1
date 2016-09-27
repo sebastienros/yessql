5b32acadc43da95ddf06a963c5800f6a20135d0f
Sebastien Ros <sebastienros@gmail.com>
2016-09-27 15:48:19 -0700

    Updating package versions

--- pack.ps1
+++ pack.ps1
@@ -1,7 +1,7 @@
 Remove-Item ".\artifacts\packages" -Recurse -ErrorAction Ignore
 
-dotnet pack .\src\YesSql.Core --configuration release --output .\artifacts\packages\YesSql.Core
-dotnet pack .\src\YesSql.Storage.InMemory --configuration release --output .\artifacts\packages\YesSql.Storage.InMemory
-dotnet pack .\src\YesSql.Storage.Cache --configuration release --output .\artifacts\packages\YesSql.Storage.Cache
-dotnet pack .\src\YesSql.Storage.LightningDB --configuration release --output .\artifacts\packages\YesSql.Storage.LightningDB
-dotnet pack .\src\YesSql.Storage.Sql --configuration release --output .\artifacts\packages\YesSql.Storage.Sql
+dotnet pack .\src\YesSql.Core -c release -o .\artifacts\packages\YesSql.Core
+dotnet pack .\src\YesSql.Storage.InMemory -c release -o .\artifacts\packages\YesSql.Storage.InMemory
+dotnet pack .\src\YesSql.Storage.Cache -c release -o .\artifacts\packages\YesSql.Storage.Cache
+dotnet pack .\src\YesSql.Storage.LightningDB -c release -o .\artifacts\packages\YesSql.Storage.LightningDB
+dotnet pack .\src\YesSql.Storage.Sql -c release -o .\artifacts\packages\YesSql.Storage.Sql
