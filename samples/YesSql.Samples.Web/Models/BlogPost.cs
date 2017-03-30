cd459a7d69495c4080e322146607c5acd05dc97a
hishamco <hishamco_2007@yahoo.com>
2017-03-30 04:20:41 +0300

    Add YesSql.Samples.Web

---
+++ samples/YesSql.Samples.Web/Models/BlogPost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace YesSql.Samples.Web.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime PublishedUtc { get; set; }
+
+        public string[] Tags { get; set; }
+    }
+}
