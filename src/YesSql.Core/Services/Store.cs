54437f98507c39d34fd58f5d718404b841d584c8
Pavel Pavlov <damn.lenber@gmail.com>
2017-04-11 15:18:40 +0300

    Updated store (table is Identifiers)
    Added primary key for table is identifiers. Primary key will be for "dimension's" column.

--- src/YesSql.Core/Services/Store.cs
+++ src/YesSql.Core/Services/Store.cs
@@ -59,7 +59,7 @@ namespace YesSql.Core.Services
                     );
 
                     builder.CreateTable(LinearBlockIdGenerator.TableName, table => table
-                        .Column<string>("dimension")
+                        .Column<string>("dimension", column => column.PrimaryKey().NotNull())
                         .Column<ulong>("nextval")
                     )
                     .AlterTable(LinearBlockIdGenerator.TableName, table => table
