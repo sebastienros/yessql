b4f3fa77b12c5dbfab4f378d63f5420dd43e2401
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2018-09-24 21:50:19 +0300

    Add nocase collation to SqLite

--- src/YesSql.Provider.Sqlite/SqliteCommandInterpreter.cs
+++ src/YesSql.Provider.Sqlite/SqliteCommandInterpreter.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
 using YesSql.Sql;
 using YesSql.Sql.Schema;
 
@@ -18,6 +22,92 @@ namespace YesSql.Provider.Sqlite
         public override IEnumerable<string> Run(IDropForeignKeyCommand command)
         {
             yield break;
+        }
+
+        public override IEnumerable<string> Run(ICreateTableCommand command)
+        {
+            var builder = new StringBuilder();
+
+            builder.Append(_dialect.CreateTableString)
+                .Append(' ')
+                .Append(_dialect.QuoteForTableName(command.Name))
+                .Append(" (");
+
+            var appendComma = false;
+            foreach (var createColumn in command.TableCommands.OfType<CreateColumnCommand>())
+            {
+                if (appendComma)
+                {
+                    builder.Append(", ");
+                }
+                appendComma = true;
+
+                Run(builder, createColumn);
+            }
+
+            var primaryKeys = command.TableCommands.OfType<CreateColumnCommand>().Where(ccc => ccc.IsPrimaryKey && !ccc.IsIdentity).Select(ccc => _dialect.QuoteForColumnName(ccc.ColumnName)).ToArray();
+            if (primaryKeys.Any())
+            {
+                if (appendComma)
+                {
+                    builder.Append(", ");
+                }
+
+                builder.Append(_dialect.PrimaryKeyString)
+                    .Append(" ( ")
+                    .Append(String.Join(", ", primaryKeys.ToArray()))
+                    .Append(" )");
+            }
+
+            builder.Append(" )");
+            yield return builder.ToString();
+        }
+
+        public override void Run(StringBuilder builder, IAddColumnCommand command)
+        {
+            if (_dialect.GetTypeName(DbType.String, 0, 0, 0) == "TEXT")
+            {
+                builder.Append(" collate nocase");
+            }
+
+            base.Run(builder, command);
+        }
+
+        private void Run(StringBuilder builder, ICreateColumnCommand command)
+        {
+            const char Space = ' ';
+            builder.Append(_dialect.QuoteForColumnName(command.ColumnName)).Append(Space);
+
+            if (_dialect.GetTypeName(DbType.String, 0, 0, 0) == "TEXT")
+            {
+                builder.Append("collate nocase");
+            }
+
+            if (!command.IsIdentity || _dialect.HasDataTypeInIdentityColumn)
+            {
+                builder.Append(_dialect.GetTypeName(command.DbType, command.Length, command.Precision, command.Scale));
+            }
+
+            if (command.IsIdentity && _dialect.SupportsIdentityColumns)
+            {
+                builder.Append(Space).Append(_dialect.IdentityColumnString);
+            }
+
+            if (command.Default != null)
+            {
+                builder.Append(" default ").Append(_dialect.GetSqlValue(command.Default)).Append(Space);
+            }
+
+            builder.Append(command.IsNotNull
+                               ? " not null"
+                               : !command.IsPrimaryKey && !command.IsUnique
+                                     ? _dialect.NullColumnString
+                                     : string.Empty);
+
+            if (command.IsUnique && _dialect.SupportsUnique)
+            {
+                builder.Append(" unique");
+            }
         }
     }
 }
