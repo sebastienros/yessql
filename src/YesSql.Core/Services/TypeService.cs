4a9d8efeab5c11fc954d773556ce9a61e300c326
Matt Barnes <mabarnes@microsoft.com>
2019-05-31 18:08:23 -0700

    Added second dictionary

--- src/YesSql.Core/Services/TypeService.cs
+++ src/YesSql.Core/Services/TypeService.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 
 namespace YesSql.Services
@@ -10,20 +9,27 @@ namespace YesSql.Services
     {
         private readonly ConcurrentDictionary<Type, string> typeNames = new ConcurrentDictionary<Type, string>();
 
+        private readonly ConcurrentDictionary<string, Type> nameTypes = new ConcurrentDictionary<string, Type>();
+
         public IEnumerable<Type> Keys { get { return typeNames.Keys; } }
 
         public IEnumerable<string> Values { get { return typeNames.Values; } }
 
         public Type ReverseLookup(string value)
         {
-            return typeNames.FirstOrDefault(t => t.Value == value).Key;
+            if (value == "dynamic")
+            {
+                return null;
+            }
+
+            return nameTypes[value];
         }
 
         public string this[Type t]
         {
             get
             {
-                return typeNames.GetOrAdd(t, type => 
+                return typeNames.GetOrAdd(t, type =>
                 {
                     var typeInfo = t.GetTypeInfo();
                     if (IsAnonymousType(typeInfo))
@@ -32,14 +38,17 @@ namespace YesSql.Services
                     }
 
                     var customName = typeInfo.GetCustomAttribute<SimplifiedTypeName>();
+                    var actualName = String.IsNullOrEmpty(customName?.Name) ? String.Concat(type.FullName, ", ", typeInfo.Assembly.GetName().Name) : customName.Name;
+                    nameTypes[actualName] = t;
 
-                    return String.IsNullOrEmpty(customName?.Name) ? String.Concat(type.FullName, ", ", typeInfo.Assembly.GetName().Name) : customName.Name;
+                    return actualName;
                 });
             }
 
             set
             {
                 typeNames[t] = value;
+                nameTypes[value] = t;
             }
         }
 
