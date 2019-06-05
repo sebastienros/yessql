09b62b4fe960661e78a56f01111a79501483e05e
Dean Marcussen <dean.marcussen@gmail.com>
2019-06-05 15:50:54 +0100

    Revert accidental change to PostgreSqlDialect

--- src/YesSql.Provider.PostgreSql/PostgreSqlDialect.cs
+++ src/YesSql.Provider.PostgreSql/PostgreSqlDialect.cs
@@ -50,6 +50,7 @@ namespace YesSql.Provider.PostgreSql
         public override string NotInOperator(string values) => " <> all(array[" + values + "])";
         public override string IdentitySelectString => "RETURNING ";
         public override string IdentityColumnString => "SERIAL PRIMARY KEY";
+        public override bool SupportsIfExistsBeforeTableName => true;
         public override bool PrefixIndex => true;
         public override bool SupportsTableNameAfterIndexName => false;
 
