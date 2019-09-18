e8155141d929590cd672f4af98b4b1630d01b06c
Sébastien Ros <sebastienros@gmail.com>
2019-09-17 18:25:26 -0700

    Updating dependencies (#200)

--- samples/YesSql.Samples.Web/Program.cs
+++ samples/YesSql.Samples.Web/Program.cs
@@ -1,5 +1,12 @@
-﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace YesSql.Samples.Web
 {
@@ -7,15 +14,11 @@ namespace YesSql.Samples.Web
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseEnvironment(EnvironmentName.Development)
-                .Build();
-
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
--- src/YesSql.Provider.SqlServer/SqlServerDbProviderOptionsExtensions.cs
+++ src/YesSql.Provider.SqlServer/SqlServerDbProviderOptionsExtensions.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Data;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 
 namespace YesSql.Provider.SqlServer
 {
