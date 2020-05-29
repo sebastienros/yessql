89049f96034e08f3ea2c54dab54d7d2ca8ce20d2
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-05-29 07:57:25 +0300

    Add System.Text.Json APIs

--- src/YesSql.Core/Serialization/JsonContentSerializer.cs
+++ src/YesSql.Core/Serialization/JsonContentSerializer.cs
@@ -1,28 +1,17 @@
-using Newtonsoft.Json;
 using System;
+using System.Text.Json;
 
 namespace YesSql.Serialization
 {
     public class JsonContentSerializer : IContentSerializer
     {
-        private readonly static JsonSerializerSettings _jsonSettings = new JsonSerializerSettings {
-            TypeNameHandling = TypeNameHandling.Auto,
-            DateTimeZoneHandling = DateTimeZoneHandling.Utc
-        };
-
         public object Deserialize(string content, Type type)
-        {
-            return JsonConvert.DeserializeObject(content, type, _jsonSettings);
-        }
+            => JsonSerializer.Deserialize(content, type);
 
         public dynamic DeserializeDynamic(string content)
-        {
-            return JsonConvert.DeserializeObject<dynamic>(content, _jsonSettings);
-        }
+            => JsonSerializer.Deserialize<dynamic>(content);
 
         public string Serialize(object item)
-        {
-            return JsonConvert.SerializeObject(item, _jsonSettings);
-        }
+            => JsonSerializer.Serialize(item);
     }
 }
