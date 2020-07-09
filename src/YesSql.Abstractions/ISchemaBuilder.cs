fc23fdf410edee9e44b18c9226f8b8f173c2ec51
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-07-09 14:09:48 +0300

    Remove other overloads

--- src/YesSql.Abstractions/ISchemaBuilder.cs
+++ src/YesSql.Abstractions/ISchemaBuilder.cs
@@ -12,8 +12,6 @@ namespace YesSql.Sql
         DbTransaction Transaction { get; }
 
         ISchemaBuilder AlterTable(string name, Action<IAlterTableCommand> table);
-        ISchemaBuilder CreateForeignKey(string name, string srcModule, string srcTable, string[] srcColumns, string destTable, string[] destColumns);
-        ISchemaBuilder CreateForeignKey(string name, string srcTable, string[] srcColumns, string destModule, string destTable, string[] destColumns);
         ISchemaBuilder CreateForeignKey(string name, string srcTable, string[] srcColumns, string destTable, string[] destColumns);
         ISchemaBuilder CreateMapIndexTable(Type indexType, Action<ICreateTableCommand> table, string collection);
         ISchemaBuilder CreateReduceIndexTable(Type indexType, Action<ICreateTableCommand> table, string collection);
