6c1eed0bff89dc397605991b1903415b251c813f
Sébastien Ros <sebastienros@gmail.com>
2020-11-25 12:56:17 -0800

    Update SqliteTests.cs

--- test/YesSql.Tests/SqliteTests.cs
+++ test/YesSql.Tests/SqliteTests.cs
@@ -110,7 +110,7 @@ namespace YesSql.Tests
                     var builder = new SchemaBuilder(_store.Configuration, transaction);
 
                     builder
-                        .DropMapIndexTable(nameof(PropertyIndex));
+                        .DropMapIndexTable<PropertyIndex>();
 
                     builder
                         .CreateMapIndexTable<PropertyIndex>(column => column
