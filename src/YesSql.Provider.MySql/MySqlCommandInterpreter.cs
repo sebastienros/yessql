77cb4d67f5bfe9120b807baa2e651fc5bff0ef32
Mike Alhayek <mike@crestapps.com>
2023-10-10 17:38:08 -0700

    remove Regex.

--- src/YesSql.Provider.MySql/MySqlCommandInterpreter.cs
+++ src/YesSql.Provider.MySql/MySqlCommandInterpreter.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using YesSql.Sql;
 using YesSql.Sql.Schema;
 
@@ -73,24 +72,18 @@ namespace YesSql.Provider.MySql
                 );
         }
 
-        private static readonly Regex ColumnNamePattern = new(@"([a-zA-Z_][a-zA-Z0-9_]+)\s*(\(\s*(\d+)\s*\))?");
-
         private string GetColumName(string name)
         {
-            var result = ColumnNamePattern.Match(name);
-            if (result.Success && result.Groups[1].Success)
+            var parts = name.Split(new[] { '(', ')', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var final = _dialect.QuoteForColumnName(parts[0]);
+
+            if (parts.Length > 1 && int.TryParse(parts[1], out var length))
             {
-                var final = _dialect.QuoteForColumnName(result.Groups[1].Value);
-
-                if (result.Groups.Count == 4 && result.Groups[3].Success)
-                {
-                    final += $"({int.Parse(result.Groups[3].Value)})";
-                }
-
-                return final;
+                final += $"({length})";
             }
 
-            return _dialect.QuoteForColumnName(name);
+            return final;
         }
     }
 }
