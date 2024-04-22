47115e3a4f18be265e4793783de6d3bb0f014dfb
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-04-22 04:10:36 +0300

    comment postgres tests

--- test/YesSql.Tests/PostgreSqlLegacyIdentityTests.cs
+++ test/YesSql.Tests/PostgreSqlLegacyIdentityTests.cs
@@ -5,34 +5,34 @@ using YesSql.Provider.PostgreSql;
 
 namespace YesSql.Tests
 {
-    public class PostgreSqlLegacyIdentityTests : PostgreSqlTests
-    {
-        public PostgreSqlLegacyIdentityTests(ITestOutputHelper output) : base(output)
-        {
-        }
+//    public class PostgreSqlLegacyIdentityTests : PostgreSqlTests
+//    {
+//        public PostgreSqlLegacyIdentityTests(ITestOutputHelper output) : base(output)
+//        {
+//        }
 
-        public override async Task InitializeAsync()
-        {
-            await PostgreSqlContainer.StartAsync();
-            await base.InitializeAsync();
-        }
+//        public override async Task InitializeAsync()
+//        {
+//            await PostgreSqlContainer.StartAsync();
+//            await base.InitializeAsync();
+//        }
 
-        protected override IConfiguration CreateConfiguration()
-        {
-            return new Configuration()
-                .UsePostgreSql(PostgreSqlContainer.GetConnectionString())
-                .SetTablePrefix(TablePrefix)
-                .UseBlockIdGenerator()
-                .SetIdentityColumnSize(IdentityColumnSize.Int32)
-                ;
-        }
+//        protected override IConfiguration CreateConfiguration()
+//        {
+//            return new Configuration()
+//                .UsePostgreSql(PostgreSqlContainer.GetConnectionString())
+//                .SetTablePrefix(TablePrefix)
+//                .UseBlockIdGenerator()
+//                .SetIdentityColumnSize(IdentityColumnSize.Int32)
+//                ;
+//        }
 
-        [Fact(Skip = "Skip to make test faster in this configuration")]
-        public override Task ShouldGateQuery()
-        {
-            return base.ShouldGateQuery();
-        }
+//        [Fact(Skip = "Skip to make test faster in this configuration")]
+//        public override Task ShouldGateQuery()
+//        {
+//            return base.ShouldGateQuery();
+//        }
 
-        public async override Task DisposeAsync() => await PostgreSqlContainer.DisposeAsync();
-    }
+//        public async override Task DisposeAsync() => await PostgreSqlContainer.DisposeAsync();
+//    }
 }
