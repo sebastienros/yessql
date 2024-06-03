5f47256420a2c6634964d17f7f8a061b345a15f9
Tony Han <huarui2219@qq.com>
2024-06-04 01:28:00 +0800

    add document details to  exception

--- src/YesSql.Abstractions/ConcurrencyException.cs
+++ src/YesSql.Abstractions/ConcurrencyException.cs
@@ -1,14 +1,28 @@
 using System;
+using System.Reflection.Metadata;
 
 namespace YesSql
 {
     public class ConcurrencyException : Exception
     {
+        private readonly string _message;
         public Document Document { get; }
 
-        public ConcurrencyException(Document document) : base($"The document with ID '{document.Id}' and type '{document.Type}' could not be updated as it has been changed by another process.")
+        public ConcurrencyException(Document document)
         {
             Document = document;
+
+            _message= $"""
+The document could not be updated as it has been changed by another process.
+     ID: {Document.Id}
+   Type: {Document.Type}
+Version: {Document.Version}
+Content: {Document.Content}        
+""";
         }
+        
+        public override string Message { get { return _message; } }
+
+
     }
 }
