093dd147ac916cafb0e8f9fefab5bb3855ec2a4c
Mike Alhayek <mike@crestapps.com>
2024-06-03 10:13:20 -0700

    fix build

--- src/YesSql.Abstractions/ConcurrencyException.cs
+++ src/YesSql.Abstractions/ConcurrencyException.cs
@@ -4,7 +4,7 @@ namespace YesSql
 {
     public class ConcurrencyException : Exception
     {
-        public readonly Document Document { get; }
+        public Document Document { get; }
 
         public ConcurrencyException(Document document) : base($"The document with ID '{document.Id}' and type '{document.Type}' could not be updated as it has been changed by another process.")
         {
