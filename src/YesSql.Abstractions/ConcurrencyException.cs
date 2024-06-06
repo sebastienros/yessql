01fea28df4f091bd8c0904c6bf514c2ad7fdd663
Tony Han <hyzx86@live.com>
2024-06-07 01:15:23 +0800

    Add custom message to ConcurrencyException (#568)
    Co-authored-by: Mike Alhayek <mike@crestapps.com>

--- src/YesSql.Abstractions/ConcurrencyException.cs
+++ src/YesSql.Abstractions/ConcurrencyException.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Reflection.Metadata;
 
 namespace YesSql
 {
     public class ConcurrencyException : Exception
     {
-        public ConcurrencyException() : base("The document could not be updated as it has been changed by another process.")
+        public Document Document { get; }
+
+        public ConcurrencyException(Document document) : base($"The document with Id '{document.Id}' and type '{document.Type}' could not be updated as it has been changed by another process.")
         {
+            Document = document;
         }
     }
 }
