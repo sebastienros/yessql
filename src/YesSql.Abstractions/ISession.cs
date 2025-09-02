253180aa8243d16261a2e80195b31a6f96e3ed99
Mike Alhayek <mike@crestapps.com>
2025-09-02 07:34:26 -0700

    fix build

--- src/YesSql.Abstractions/ISession.cs
+++ src/YesSql.Abstractions/ISession.cs
@@ -134,7 +134,6 @@ namespace YesSql
         Task<DbTransaction> BeginTransactionAsync(IsolationLevel isolationLevel, CancellationToken cancellationToken = default);
 
         /// <summary>
-
         /// Returns the current <see cref="DbTransaction"/> if it exists.
         /// </summary>
         DbTransaction CurrentTransaction { get; }
